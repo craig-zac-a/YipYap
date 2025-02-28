9b2d024ea868d24aba9633ee04c65ae7c664b85d
Carson T <carson.treece@ku.edu>
2025-02-27 22:01:11 -0600

    Created app home screen and screen navigation
    Read online that you should use a stack navigator to go through pages. Set up a base launch screen for the app and initialized the stack navigator. Also created a test CreateAccount page to ensure that page navigation works.

---
+++ app/CreateAccount.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function CreateAccount({ navigation }: any) {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Create Account</ThemedText>
+
+      {/* <View style={styles.buttonContainer}>
+        <Button title="Back to Home" onPress={() => navigation.goBack()} />
+      </View> */}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputContainer: {
+    width: '100%',
+    maxWidth: 300,
+    gap: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    gap: 16,
+    width: '100%',
+    maxWidth: 300,
+    alignItems: 'center',
+  },
+});

b0ba68fe37c19c6e91a362fd127e8c9c38be5386
Carson T <carson.treece@ku.edu>
2025-02-28 16:49:44 -0600

    Created an empty login page

--- app/AppNavigator.tsx
+++ app/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import CreateAccount from './CreateAccount';
+import Login from './Login';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export default function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
+
       <Stack.Screen 
         name="CreateAccount" 
         component={CreateAccount} 
@@ -16,6 +18,15 @@ export default function AppNavigator() {
           //headerLeft: () => null
         }} 
       />
+
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={{
+          //headerLeft: () => null
+        }} 
+      />
+
     </Stack.Navigator>
   );
 }
